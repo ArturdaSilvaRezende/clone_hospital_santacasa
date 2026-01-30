f608f72f067ba763efc5af8e3b7a589392eb3d50
Artur da Silva Rezende <artursrezende@gmail.com>
2026-01-30 11:27:51 -0300

    feat: refactor CallToActionSections layout and styles for improved responsiveness

--- src/pages/Home/components/CallToAction/index.jsx
+++ src/pages/Home/components/CallToAction/index.jsx
@@ -4,35 +4,35 @@ import CustomLink from '~/components/CustomComponents/Link'
 export default function CallToActionSections() {
   return (
     <section
-      className="flex w-full flex-col md:flex-row"
+      className="flex w-full"
       aria-label="Chamadas para ação de voluntariado e doação"
     >
       <article
-        className="flex flex-1 flex-col items-start justify-center bg-[#31313F] px-8 py-16 md:px-12 lg:px-16"
+        className="flex w-[50%] items-center justify-center bg-[#31313F]"
         aria-labelledby="volunteer-heading"
       >
-        <div className="flex max-w-md flex-col items-center">
+        <div className="mx-auto flex h-67 w-110 flex-col items-center justify-center">
           <h2
             id="volunteer-heading"
-            className="mb-8 w-120 text-[28px] leading-relaxed font-normal text-white"
+            className="mb-5 text-[26px] leading-relaxed font-medium text-white"
           >
             Para saber como se tornar um voluntário(a) clique no botão abaixo.
           </h2>
           <CustomLink
             label="Acessar"
-            classNameContainer="bg-[#FD0003] hover:bg-red-700  text-white w-[158px] mr-auto -ml-3"
+            classNameContainer="bg-[#FD0003] hover:bg-red-700  text-white px-[49px] mr-auto"
           />
         </div>
       </article>
 
       <article
-        className="flex flex-1 flex-col items-start justify-center bg-[#FDE4E1] px-8 py-16 md:px-12 lg:px-16"
+        className="flex w-[50%] items-center justify-center bg-[#FDE4E1]"
         aria-labelledby="donation-heading"
       >
-        <div className="flex max-w-md flex-col items-center">
+        <div className="mx-auto flex h-67 w-96.5 flex-col items-center justify-center">
           <h2
             id="donation-heading"
-            className="mb-8 w-100 text-[28px] leading-relaxed font-normal text-[#2F2E41]"
+            className="mb-5 text-[26px] leading-relaxed font-medium text-[#2F2E41]"
           >
             Transforme vidas com sua doação!{' '}
             <span className="font-semibold text-red-600">
@@ -42,7 +42,7 @@ export default function CallToActionSections() {
 
           <CustomLink
             label="Acessar"
-            classNameContainer="bg-[#FD0003] hover:bg-red-700  text-white w-[158px] mr-auto"
+            classNameContainer="bg-[#FD0003] hover:bg-red-700  text-white px-[49px] mr-auto"
           />
         </div>
       </article>
