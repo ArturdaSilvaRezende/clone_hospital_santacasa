a7c1ddc5f6858a6b0c1223a9269a585ded8ed9dd
Artur da Silva Rezende <artursrezende@gmail.com>
2026-03-06 22:59:01 -0300

    feat: add layout and visiting hours components for Horário de Visitas page

---
+++ src/app/horario-de-visitas/layout.jsx
@@ -0,0 +1,10 @@
+import '../globals.css'
+
+export const metadata = {
+  title: 'Santa casa | Horário de visitas',
+  description: ''
+}
+
+export default function Layout({ children }) {
+  return <main>{children}</main>
+}
---
+++ src/app/horario-de-visitas/page.jsx
@@ -0,0 +1,11 @@
+import CallToActionSections from '~/components/CallToAction'
+import VisitingHours from '~/view/pages/HorarioDeVisita/Tabs'
+
+export default function HorarioDeVisitas() {
+  return (
+    <>
+      <VisitingHours />
+      <CallToActionSections />
+    </>
+  )
+}
