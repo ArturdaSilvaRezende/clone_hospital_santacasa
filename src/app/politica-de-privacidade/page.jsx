fd8ec78db8a1b1ef6bd60e9085d3fbb7db954841
Artur da Silva Rezende <artursrezende@gmail.com>
2026-03-09 16:24:16 -0300

    feat: add Política de Privacidade page and layout components

---
+++ src/app/politica-de-privacidade/page.jsx
@@ -0,0 +1,12 @@
+import CallToActionSections from '~/components/CallToAction'
+import PrivacyPolicy from '~/view/pages/politicaDePrivacidade/Content'
+
+
+export default function PoliDePrivacidade() {
+  return (
+    <>
+      <PrivacyPolicy />
+      <CallToActionSections />
+    </>
+  )
+}
---
+++ src/view/pages/politicaDePrivacidade/Content/index.jsx
@@ -0,0 +1,214 @@
+export default function PrivacyPolicy() {
+  return (
+    <section
+      aria-label="Política de Privacidade"
+      className="my-16 max-sm:mt-10 max-sm:mb-14"
+    >
+      <div className="container mx-auto max-sm:px-6 md:px-8 lg:px-8 xl:px-0">
+        <article className="rounded-2xl bg-white p-6 max-sm:p-4" role="article">
+          <header className="mb-8">
+            <h1 className="text-center text-[24px] font-semibold text-[#262626]">
+              Política de Privacidade
+            </h1>
+          </header>
+
+          <div className="space-y-4">
+            <section aria-label="Apresentação" className="space-y-2.5">
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                Apresentação
+              </h2>
+
+              <p className="text-[16px] font-normal text-[#535353]">
+                Cientes da importância e da responsabilidade de guardar com
+                segurança e total confidencialidade as informações fornecidas,
+                empenhamos tempo e investimentos para criar normas e processos
+                internos que garantam a sua privacidade e segurança total dos
+                seus dados em todas as etapas e procedimentos administrativos da
+                empresa. A seguir você conhecerá quais informações são coletadas
+                e como estas informações são tratadas na loja.
+              </p>
+
+              <h3 className="text-[16px] font-medium text-[#FD0003]">
+                A informações a seguir são referentes ao nosso bazar on-line e
+                aos dados coletados em todo o site.
+              </h3>
+            </section>
+
+            <section
+              aria-label="Porque precisamos coletar algumas informações?"
+              className="space-y-2.5"
+            >
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                Porque precisamos coletar algumas informações?
+              </h2>
+
+              <p className="text-[16px] font-normal text-[#535353]">
+                Da mesma forma que nas compras em lojas tradicionais do mundo
+                físico precisamos fornecer algumas informações para concluir o
+                processo de compra, na internet ocorre da mesma forma, alguns
+                dados pessoais como nome completo, CPF, endereço completo de
+                entrega, enfim, todas as informações necessárias para
+                identificar quem está comprando e proceder com a entrega. Cabe
+                ainda ressaltar que sem estes dados fica impossível a emissão de
+                nota fiscal e liberação dos pedidos.
+              </p>
+
+              <p className="text-[16px] font-normal text-[#535353]">
+                Solicitamos informações quando você:
+              </p>
+
+              <ul className="list-disc space-y-1 pl-6">
+                <li className="text-[16px]">
+                  <strong className="font-medium text-[#262626]">
+                    Cadastra-se como cliente, para agilização do processo.
+                  </strong>
+                </li>
+
+                <li className="text-[16px]">
+                  <strong className="font-medium text-[#262626]">
+                    Efetiva um pedido.
+                  </strong>
+                </li>
+
+                <li className="text-[16px]">
+                  <strong className="font-medium text-[#262626]">
+                    Responde uma pesquisa online.
+                  </strong>
+                </li>
+
+                <li className="text-[16px]">
+                  <strong className="font-medium text-[#262626]">
+                    Participa de uma promoção.
+                  </strong>
+                </li>
+              </ul>
+            </section>
+
+            <section
+              aria-label="Quais informações são coletadas?"
+              className="space-y-2.5"
+            >
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                Quais informações são coletadas?
+              </h2>
+
+              <p className="text-[16px] font-normal text-[#535353]">
+                Para poder realizar pedidos, há necessidade da realização de um
+                cadastro de clientes, Os dados solicitados são mantidos em
+                completo sigilo. Não desejamos nem pretendemos invadir sua
+                privacidade durante sua visita ao nosso site.
+              </p>
+            </section>
+
+            <section
+              aria-label="E os meus dados e numero do CPF?"
+              className="space-y-2.5"
+            >
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                E os meus dados e numero do CPF?
+              </h2>
+
+              <p className="mb-5 text-[16px] font-normal text-[#535353]">
+                A loja possuí uma política concreta de completo sigilo quanto
+                aos seus dados e número de CPF. Eles são solicitados no cadastro
+                do site para verificação de seus dados em nossa loja física, e
+                agilizar o processo de entrega do produto adquirido por você.
+              </p>
+            </section>
+
+            <section
+              aria-label="O que será feito com essas informações?"
+              className="space-y-2.5"
+            >
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                O que será feito com essas informações?
+              </h2>
+
+              <p className="text-[16px] font-normal text-[#535353]">
+                Dados pessoais: São utilizados exclusivamente para emissão de
+                pedidos e futuros contatos para informá-lo sobre atualizações de
+                produtos, ofertas especiais e novos serviços da loja.
+              </p>
+              <p className="text-[16px] font-normal text-[#535353]">
+                A nossa loja se reserva o direito de produzir análises
+                estatísticas agregadas de características ou comportamento de
+                seus usuários, objetivando a medição de interesse na procura de
+                produtos e serviços colocados à disposição em nosso site.
+              </p>
+              <p className="text-[16px] font-normal text-[#535353]">
+                Tais análises poderão, a exclusivo critério de nossa loja, ser
+                informadas aos seus parceiros comerciais, utilizadas em material
+                promocional ou publicitário, publicadas no site sob forma de
+                notícia ou citadas em releases ou matérias jornalísticas. Mas
+                nunca jamais citando dados pessoais dos usuários.
+              </p>
+            </section>
+
+            <section
+              aria-label="O que não será feito com essas informações?"
+              className="space-y-2.5"
+            >
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                O que não será feito com essas informações?
+              </h2>
+
+              <ul className="list-disc space-y-1 pl-6">
+                <li className="text-[16px] font-normal text-[#535353]">
+                  Em hipótese alguma nosso banco de dados será fornecido a
+                  terceiros, salvo em cumprimento a alguma ordem judicial.
+                </li>
+                <li className="text-[16px] font-normal text-[#535353]">
+                  Negligência no seu manuseio.
+                </li>
+
+                <li className="text-[16px] font-normal text-[#535353]">
+                  Explorar de outra forma que não seja para manter o contato e o
+                  relacionamento com cliente.
+                </li>
+              </ul>
+            </section>
+
+            <section aria-label="Segurança" className="space-y-2.5">
+              <h2 className="text-[22px] font-medium text-[#2F2E41]">
+                Segurança
+              </h2>
+
+              <p className="mb-5 text-[16px] font-normal text-[#535353]">
+                No ambiente Virtual: A loja virtual é protegida por moderno
+                sistema de cadastro restrito. Apenas quem possuí os dados
+                necessários para “Entrar” na administração do sistema pode
+                visualizar pedidos. Sem isso é impossível ter acesso às
+                informações.
+              </p>
+              <p className="mb-5 text-[16px] font-normal text-[#535353]">
+                No ambiente Físico: Fazemos uso de modernos métodos e
+                tecnologias que permitem manuseio das informações com todo o
+                cuidado necessário para impedir que a informação saia da
+                empresa, sendo o acesso restrito a funcionários atráves de
+                autenticação com uso de login e senha de acesso, não é permitido
+                a gravação em discos removíveis, a impressão de documentos com
+                informações dos clientes é restrito a lista de produtos para
+                finalizar o pedido e passam por rigoroso controle.
+              </p>
+              <p className="mb-5 text-[16px] font-normal text-[#535353]">
+                Fique tranquilo, tratamos os seus dados como sendo parte dos
+                ativos da empresa, tão importante quanto os segredos comerciais,
+                estratégias, propriedades intelectuais, etc… e estamos
+                envolvidos com este compromisso.
+              </p>
+              <p className="mb-5 text-[16px] font-normal text-[#535353]">
+                Tudo isto é feito para que você possa ter ainda mais conforto e
+                tranquilidade ao comprar em nosso site.
+              </p>
+
+              <h3 className="text-[16px] font-medium text-[#FD0003]">
+                As informações desta página podem ser alteradas sem a
+                necessidade de prévio aviso.Verifique futuras atualizações.
+              </h3>
+            </section>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
+}
