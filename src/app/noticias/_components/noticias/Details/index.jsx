4fb0ed0884687725f168bda9de9d0ef0c061438d
Artur da Silva Rezende <artursrezende@gmail.com>
2026-05-20 15:08:47 -0300

    refactor: Clean up code formatting and remove unnecessary semicolons

--- src/app/noticias/_components/noticias/Details/index.jsx
+++ src/app/noticias/_components/noticias/Details/index.jsx
@@ -1,17 +1,15 @@
-
-
 export default async function Details({ params }) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API}/api/news/${params.id}`,
-    { cache: "no-store" }
-  );
+    { cache: 'no-store' }
+  )
 
-  const news = await res.json();
+  const news = await res.json()
 
   return (
     <div>
       <h1>{news.title}</h1>
       <p>{news.content}</p>
     </div>
-  );
+  )
 }
